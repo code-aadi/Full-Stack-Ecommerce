c48feea2a44a358d59a086fbdba6d4045a463b93
Code-aadi <kouravaditya7@gmail.com>
2026-08-20 18:09:16 +0530

    Changed username

--- Backend/Controller/productController.js
+++ Backend/Controller/productController.js
@@ -1,13 +1,6 @@
 import Product from "../Model/productModel.js"
 
-export const getProducts = async(req,res) =>{
-try {
-    const products = await Product.find()
-   res.json(products)
-} catch (error) {
-    console.log(error.message)
-}
-}
+
 
 
 
